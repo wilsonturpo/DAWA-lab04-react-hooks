74d9db341f284744f33500d4c67ad938d335304f
wilsonturpo <wilson.turpo@tecsup.edu.pe>
2021-04-14 22:51:22 -0500

    Quinto commit-conditional render

--- votaciones/src/components/Statistics.js
+++ votaciones/src/components/Statistics.js
@@ -5,14 +5,21 @@ const Statistics = (props) => {
   return (
     <div style={{margin:20}}>    
 
-      <h2>Statistics</h2>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.total}</p>
+      <h2>Statistics</h2>      
 
-      <p>Average: {props.average}</p>            
-      <p>Positive: {props.positive}</p>
+      {props.all ==0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+            <p>Good: {props.good}</p>
+            <p>Neutral: {props.neutral}</p>
+            <p>Bad: {props.bad}</p>
+            <p>All: {props.all}</p>
+
+            <p>Average: {props.average}</p>            
+            <p>Positive: {props.positive}</p>
+        </div>
+      )}
 
       
     </div>
