17d2b750defc248feed30097cc4dcbeec9f32db2
wilsonturpo <wilson.turpo@tecsup.edu.pe>
2021-04-14 22:02:07 -0500

    Segundo commit-creación de estados y funciones de estados

--- votaciones/src/App.js
+++ votaciones/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  //Función de Good
+  const handleGood = () =>{
+    setGood( good + 1)
+  }
+
+
+  //Función de Neutral
+  const handleNeutral = ()=>{
+    setNeutral(neutral + 1)
+  }
+
+  //Función de Bad
+  const handleBad = () =>{
+    setBad( bad + 1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:20}}>
+      <h1>Give feedback</h1>
+      <div>
+        <button onClick={handleGood} style={{margin:10}}>Good</button> 
+        <button onClick={handleNeutral} style={{margin:10}}>Neutral</button>
+        <button onClick={handleBad} style={{margin:10}}>Bad</button>
+      </div>
+
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
-  );
+  )
 }
 
 export default App;
